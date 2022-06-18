218542bbee8b5a6431c68bf0b00147f9cae054c4
ashzest <62052199+ashzest@users.noreply.github.com>
2022-06-18 22:17:14 +0530

    changes to connect to database

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+    import { initializeApp } from 'firebase/app';
+    import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,16 +16,18 @@
     measurementId: "G-74F1WSHG7G"
   };
 
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig)
+  
 
 // Initiliaze variables
-const auth = firebase.auth()
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const database = firebase.database()
 
 // Register Function 
-function register(){
+function register() {
   
+  alert("hi");
+
   email=document.getElementById('email').value
   password=document.getElementById('password').value
   full_name=document.getElementById('full_name').value
@@ -36,41 +37,41 @@ function register(){
 
 if(validate_email(email)==false || validate_password(password)==false)
 {
-  alert("Email or password invalid")
-  return
+  alert("Email or password invalid");
+  return;
 }
 
 //  Moving on with Authentication
 
-auth.createUserWithEmailAndPassword(email,password).
+auth.createUserWithEmailAndPassword(auth,email,password).
 then(function(){
 
 
-  var user=auth.currentUser
+  var user=auth.currentUser;
   
-  var database_ref=database.ref()
+  var database_ref=database.ref();
 
   var user_data = {
     email:email,
     full_name:full_name,
     last_login:Date.now()
-  }
+  };
 
   // Push to firebase database 
 
-  database_ref.child('users/' + user.uid).set(user_data)
+  database_ref.child('users/' + user.uid).set(user_data);
 
   // User created
 
-  alert("User created!")
+  alert("User created!");
 
 })
 .catch(function(error){
    
-  var error_code=error.code
-  var error_message=error.message
+  var error_code=error.code;
+  var error_message=error.message;
 
-  alert(error_message)
+  alert(error_message);
 })
 }
 
@@ -82,9 +83,9 @@ function validate_email(email){
 
   if(expression.test(email)==true)
   {
-    return true
+    return true;
   }
-  else return false
+  else {return false;}
 }
 
 function validate_password(password){
@@ -92,16 +93,16 @@ function validate_password(password){
   // Firebase only accepts passwords that are greater than 6 characters
 
   if(password<6)
-  return false
+  return false;
 
-  else return true
+  else {return true;}
 }
 
 function validate_field(full_name)
 {
   if(full_name==null || full_name.length<=0)
-  return false 
-  else return true
+  return false; 
+  else {return true;}
 } 
 
 
